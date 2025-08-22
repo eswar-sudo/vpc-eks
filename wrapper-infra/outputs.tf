aa88ace070b5ae63d3528ccd465886a65daa98f0
eswar-sudo <eswarkumar501@gmail.com>
2025-08-22 11:38:48 +0530

    Update outputs.tf

--- wrapper-infra/outputs.tf
+++ wrapper-infra/outputs.tf
@@ -1,16 +1,19 @@
-# VPC outputs
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  value = module.vpc.private_subnet_ids
 }
 
-# EKS outputs
 output "eks_cluster_name" {
-  value = module.eks.cluster_id
+  value = module.eks.cluster_name
 }
-output "eks_node_group" {
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_node_groups" {
   value = module.eks.node_groups
 }
