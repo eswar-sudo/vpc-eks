2378cde9326d0390f6321a391982013429e49e49
eswar-sudo <eswarkumar501@gmail.com>
2025-08-22 11:35:43 +0530

    Update outputs.tf

--- wrapper-infra/outputs.tf
+++ wrapper-infra/outputs.tf
@@ -9,9 +9,8 @@ output "private_subnets" {
 
 # EKS outputs
 output "eks_cluster_name" {
-  value = module.eks.cluster_name
+  value = module.eks.cluster_id
 }
-
 output "eks_node_group" {
-  value = module.eks.node_group_name
+  value = module.eks.node_groups
 }
